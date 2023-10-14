0d957ccab3d710d4b031b83663463468c6e7e506
rextse04 <64886175+rextse04@users.noreply.github.com>
2023-10-14 11:51:48 +0800

    Update valx.py

--- valx.py
+++ valx.py
@@ -1,2 +1,34 @@
-def strategy(message):
-    
+def arbitrage(buy_order, sell_order, order):
+    while True:
+        if buy_order[0][0] - sell_order[0][0] > 10:
+            size = min(buy_order[0][1], sell_order[0][1], 10)
+            sell_order[0][1] -= size
+            if(sell_order[0][1] == 0) sell_order.pop(0)
+            buy_order[0][1] -= size
+            if(buy_order[0][1] == 0) buy_order.pop(0)
+            order.append({
+                "type": "add",
+                "symbol": sell_order["symbol"],
+                "dir": "BUY",
+                "price": sell_order[i][0],
+                "size": size
+            })
+            order.append({
+                "type": "convert",
+                "symbol": buy_order["symbol"],
+                "dir": "BUY",
+                "size": size
+            })
+            order.append({
+                "symbol": buy_order["symbol"],
+                "dir": "SELL",
+                "price": buy_order[i][0],
+                "size": size
+            })
+        else:
+            break
+def strategy(message_vale, message_valbz):
+    order = []
+    arbitrage(message_vale, message_valbz, order)
+    arbitrage(message_valbz, message_vale, order)
+    return order
