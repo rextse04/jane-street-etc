812a2b2da8c2e65f4177b1f289ebbe6f70c2f8e2
NikabroYO <147839516+NikabroYO@users.noreply.github.com>
2023-10-14 10:57:19 +0800

    Update Bond.py

--- Bond.py
+++ Bond.py
@@ -6,27 +6,39 @@ def bond_strategy(buy: List[RestingOrder], sell: List[RestingOrder]) -> List[Tra
         These trades will need `order_id` to be added separately.
 
     Args:
-
         buy (List[RestingOrder]): Resting buy orders of BOND.
         sell (List[RestingOrder]): Resting sell orders of BOND.
 
     Returns:
-
         List[Trade]: List of trades to perform. May be empty.
-    
-    Example:
 
+    Example:
         >>> bond_strategy([[1001, 4], [999, 8]], [[998, 5], [1002, 7]])
         [{"type": "add", "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 4},
         {"type": "add", "symbol": "BOND", "dir": "BUY", "price": 998, "size": 5}]
-
     """
-    buy_trades = []
-    for i in range(len(buy)):
-        if buy[i][0] > 1000:
-            buy_trades.append({"type": Action.ADD, "symbol": Symbol.BOND, "dir": Direction.SELL, "price": buy[i][0], "size": buy[i][1]})
-    sell_trades = []
-    for i in range(len(sell)):
-        if sell[i][0] < 1000:
-            sell_trades.append({"type": Action.ADD, "symbol": Symbol.BOND, "dir": Direction.BUY, "price": sell[i][0], "size": sell[i][1]})
-    return buy_trades + sell_trades
+    trades = []
+
+    for order in buy:
+        if order[0] > 1000:
+            trade = {
+                "type": Action.ADD,
+                "symbol": Symbol.BOND,
+                "dir": Direction.SELL,
+                "price": order[0],
+                "size": order[1]
+            }
+            trades.append(trade)
+
+    for order in sell:
+        if order[0] < 1000:
+            trade = {
+                "type": Action.ADD,
+                "symbol": Symbol.BOND,
+                "dir": Direction.BUY,
+                "price": order[0],
+                "size": order[1]
+            }
+            trades.append(trade)
+
+    return trades
