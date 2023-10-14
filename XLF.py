24c6326d3bb1641d24aef512d5e9f4f6e0c94178
NikabroYO <147839516+NikabroYO@users.noreply.github.com>
2023-10-14 12:45:58 +0800

    Update XLF.py

--- XLF.py
+++ XLF.py
@@ -6,7 +6,7 @@ from helper import *
 from etc_types import *
 from typing import List
 
-def ema_strategy(vale_trade_info: List[TradeInfo], valbz_trade_info: List[TradeInfo]) -> List[Trade]:
+def ema_strategy(XLF_trade_info: List[TradeInfo]) -> List[Trade]:
    
 
     This algorithm makes use of exponential moving averages.
@@ -18,10 +18,9 @@ def ema_strategy(vale_trade_info: List[TradeInfo], valbz_trade_info: List[TradeI
         List[Trade]: A list of trades to perform. May be empty.
 
     """
-    if len(vale_trade_price_list) >= 10 and len(valbz_trade_price_list) >= 10:
-        vale: List[int] = vale_trade_price_list[-10:]
-        valbz: List[int] = valbz_trade_price_list[-10:]
-        result: List[Union[bool, float]] = adr_signal(valbz, vale)
+  
+        XLF_trade_price_list: List[int] = list(map(lambda x: x[0], XLF_trade_info))
+        result: List[str] = generate_signals(XLF_trade_price_list)
         if result:
       
             return [{"type" : Action.ADD, "symbol": Symbol.XLF, "dir" : Direction.BUY, "price": result[1]+1, "size": 10},
@@ -34,7 +33,7 @@ def ema(values: List[float], period: int) -> float:
     values: np.ndarray = np.array(values)
     return pd.DataFrame(values).ewm(span=period, adjust=False).mean().values[-1][-1]
 
-def generate_signals(values: List[float], period: int) -> List[str]:
+def generate_signals(values: List[float]) -> str:
     """Generates buy and sell signals based on EMA 5 and EMA 13 crossover.
 
     Args:
@@ -44,16 +43,12 @@ def generate_signals(values: List[float], period: int) -> List[str]:
     Returns:
         List[str]: A list of signals indicating buy ('BUY'), sell ('SELL'), or no action ('HOLD').
 
-    Example:
-        >>> values = [100, 102, 101, 105, 104, 108, 107, 109]
-        >>> signals = generate_signals(values, 5)
-        >>> print(signals)
-        ['HOLD', 'HOLD', 'HOLD', 'BUY', 'HOLD', 'BUY', 'HOLD', 'SELL']
+
     """
     ema5 = ema(values, 5)
     ema13 = ema(values, 13)
 
-    signals = []
+    
     for i in range(len(values)):
         if i >= 1:
             prev_ema5 = ema(values[:i], 5)
