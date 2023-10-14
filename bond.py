3c5527bdac1f317b21ba8ddc1cbfa23916fec72a
mosessswk <114079188+mosessswk@users.noreply.github.com>
2023-10-14 12:31:57 +0800

    Update bond.py

--- bond.py
+++ bond.py
@@ -1,1 +1,24 @@
-
+def strategy(message):
+    order = []
+    if message["buy"][0][0] > 1000:
+        trade = {
+            "type": "add",
+            "symbol": "BOND",
+            "dir": "SELL",
+            "price": message["BUY"][0][0],
+            "size": 1
+        }
+        for i in range(message["buy"][0][1]):
+            order.append(trade)
+    elif message["sell"][0][0] < 1000:
+        trade = {
+            "type": "add",
+            "symbol": "BOND",
+            "dir": "BUY",
+            "price": message["SELL"][0][0],
+            "size": 1
+        }
+        for i in range(message["sell"][0][1]):
+            order.append(trade)
+    return order
+            
