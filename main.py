f7e305d234820b9855a06368f3f32e8416c8a7d7
rextse04 <64886175+rextse04@users.noreply.github.com>
2023-10-14 12:40:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ team_name = "MARV"
 # code is intended to be a working example, but it needs some improvement
 # before it will start making good trades!
 
-id = 1
+id = 0
 def main():
     args = parse_arguments()
 
