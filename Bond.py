800740b0c977d161748954f1f3274bef3bd76e58
NikabroYO <147839516+NikabroYO@users.noreply.github.com>
2023-10-14 11:47:47 +0800

    Update Bond.py

--- Bond.py
+++ Bond.py
@@ -3,18 +3,11 @@ from etc_types import *
 
 def bond__trade_strategy(buy: List[Resting_order], sell: List[Resting_order]) -> List[Trade]:
     """Generates trades based on resting buy and sell orders for BOND
-
     Args:
-        buy (List[Resting_order]): Resting buy orders of BOND.
-        sell (List[Resting_order]): Resting sell orders of BOND.
-
+        buy (List[Resting_order]): Resting buy orders
+        sell (List[Resting_order]): Resting sell orders 
     Returns:
         List[Trade]: List of trades to perform. May be empty.
-
-    Example:
-        >>> bond_strategy([[1001, 4], [999, 8]], [[998, 5], [1002, 7]])
-        [{"type": "add", "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 4},
-        {"type": "add", "symbol": "BOND", "dir": "BUY", "price": 998, "size": 5}]
     """
     trade_orders = []
 
@@ -26,7 +19,7 @@ def bond__trade_strategy(buy: List[Resting_order], sell: List[Resting_order]) ->
                 "symbol": Symbol.BOND,
                 "dir": Direction.SELL,
                 "price": order[0],
-                "size": order[1]
+                "size": 1
             }
             trade_orders.append(trade)
 
@@ -37,7 +30,7 @@ def bond__trade_strategy(buy: List[Resting_order], sell: List[Resting_order]) ->
                 "symbol": Symbol.BOND,
                 "dir": Direction.BUY,
                 "price": order[0],
-                "size": order[1]
+                "size": 1
             }
             trade_orders.append(trade)
 
